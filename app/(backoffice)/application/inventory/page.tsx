809f241ca102a27c2fa3fcf9a5921532a9d85331
Jose Ugal <jugal@codefastly.net>
2023-11-07 14:56:03 +0100

    feat: add images on inventory page

--- app/(backoffice)/application/inventory/page.tsx
+++ app/(backoffice)/application/inventory/page.tsx
@@ -1,4 +1,5 @@
 import { InventoryCard, InventoryFixedHeader } from '@/sections/inventory'
+import Image from 'next/image'
 
 export default function BackofficeApplicationInventoryLayout () {
   return (
@@ -10,22 +11,50 @@ export default function BackofficeApplicationInventoryLayout () {
           title="Item groups"
           description="Create multiple variants of the same item using Item Groups"
           buttonText="New Item Group"
-        />
+        >
+          <Image
+            src={'/inventory/inventory01.svg'}
+            width={100}
+            height={100}
+            alt=''
+          />
+        </InventoryCard>
         <InventoryCard
           title="Items"
           description="Create standalone items and services that you buy and sell"
           buttonText="New Item"
-        />
+        >
+          <Image
+            src={'/inventory/inventory02.svg'}
+            width={100}
+            height={100}
+            alt=''
+          />
+        </InventoryCard>
         <InventoryCard
           title="Composite Items"
           description="Bundle different items together and sell them as kits"
           buttonText="New Composite Item"
-        />
+        >
+          <Image
+            src={'/inventory/inventory03.svg'}
+            width={100}
+            height={100}
+            alt=''
+          />
+        </InventoryCard>
         <InventoryCard
           title="Price Lists"
           description="Tweak your item prices for specific contacts or transactions"
           buttonText="Enable Now"
-        />
+        >
+          <Image
+            src={'/inventory/inventory04.svg'}
+            width={100}
+            height={100}
+            alt=''
+          />
+        </InventoryCard>
       </div>
     </div>
   )
